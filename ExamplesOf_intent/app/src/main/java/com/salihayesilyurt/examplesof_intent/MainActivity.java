166922ad3459cb61486cb05106386ca9ebad7c46
ZoneLearning <myselflearningzone@gmail.com>
2016-02-25 11:29:48 +0200

    Foto çekme özelliği eklendi. Ama geri döndürme çalışmıyor. Tekrar bakılacak.

--- ExamplesOf_intent/app/src/main/java/com/salihayesilyurt/examplesof_intent/MainActivity.java
+++ ExamplesOf_intent/app/src/main/java/com/salihayesilyurt/examplesof_intent/MainActivity.java
@@ -1,9 +1,16 @@
 package com.salihayesilyurt.examplesof_intent;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnEtkinlikOlustur;
     private Button btnResimVideoCek;
+
+    private ImageView img;
+    private Button btnResimCek;
+    private ResimVideoCek resimVideoCek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         SayacOlustur sayacOlustur = new SayacOlustur(this, etSayac, btnSayac);
 /*----------------------------------------------------------------------------------*/
         EtkinlikOlustur etkinlikOlustur = new EtkinlikOlustur(this, btnEtkinlikOlustur);
-        //ResimVideoCek resimvideocek = new ResimVideoCek(this,btnResimVideoCek);
+/*----------------------------------------------------------------------------------*/
+        resimVideoCek = new ResimVideoCek(this, img, btnResimVideoCek);
     }
 
     private void initialize() {
@@ -43,6 +55,32 @@ public class MainActivity extends AppCompatActivity {
         btnSayac = (Button) findViewById(R.id.btn_sayac);
         btnEtkinlikOlustur = (Button) findViewById(R.id.btn_etkinlikolustur);
         btnResimVideoCek = (Button) findViewById(R.id.btnFotoVideo);
+        btnResimCek = (Button) findViewById(R.id.btnFotoVideo);
+        img = (ImageView) findViewById(R.id.imgFotoVideo);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resimVideoCek.getKod() == requestCode && resultCode == RESULT_OK) {
+            //  reqCode == CAMERA_REQ_CODE && resCode ==)
+            //req 1 dönüyor eger kameramdan donenle 1se o zaman doğru
+            //Kaydete bastı mı yoksa direkt mi çıktı sorununu .çzmek için
+            //Bitmap thumb = (Bitmap)intent.getExtras().get("data");
+            //rsm.setResim(thumb);
+
+            Bitmap thump = null;
+            try {
+                thump = MediaStore.Images.Media.getBitmap(getContentResolver(), resimVideoCek.getUri());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            resimVideoCek.setResim(thump);
+        }
+    }
 }
---
+++ ExamplesOf_intent/app/src/main/java/com/salihayesilyurt/examplesof_intent/ResimVideoCek.java
@@ -0,0 +1,89 @@
+package com.salihayesilyurt.examplesof_intent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by SalihaYesilyurt on 25.2.2016.
+ */
+public class ResimVideoCek implements View.OnClickListener {
+
+    private Activity act;
+    private Button btn;
+    private ImageView img;
+    private final int CAMERA_REQ_CODE = 1;
+    private Uri imgUri;
+
+    public ResimVideoCek(Activity activity,ImageView ImageView, Button button){
+
+        // Context in amacı Activity yi kullanabilmek.
+        //Context Activity ile değiltirildi
+        act = activity;
+        btn = button;
+        img = ImageView;
+        btn.setOnClickListener(this);
+        imgUri=makeUri("resim");
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (btn.getId() == v.getId()) {
+            Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            //Resim çekip getirecek olan kapalı intent
+            //Kamerayı açtırıyor
+            //Uri imageUri =makeUri("ressam");// imgUri yapınca gerek kalmadı
+
+            if(imgUri!=null) {
+                cameraintent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                //Uri oluştur
+            }
+
+            act.startActivityForResult(cameraintent, CAMERA_REQ_CODE);
+            //Con ile çağıramadık çünkü context ile arasında kalmış Context yerine activity alacagiz
+        }
+
+    }
+
+    public Uri makeUri(String fileName) {
+        //içine dosya ismini alacak, uri yapıp geri döndürecek
+        //Uri aracı
+
+        try {
+            File hafizakarti = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            File image = null;
+            image = File.createTempFile(fileName, ".jpg", hafizakarti);
+            return Uri.fromFile(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return null;
+            //En son null döndüyoruz kod derlesin diye
+
+        }
+    }
+
+    public int getKod (){
+        return CAMERA_REQ_CODE; //Diğer tarafa reg codu alabilcek
+    }
+
+    public Uri getUri () {
+        return imgUri;
+    }
+
+    public void setResim(Bitmap resim) {
+        img.setImageBitmap(resim);
+    }
+}
